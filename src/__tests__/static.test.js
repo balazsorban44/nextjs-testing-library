7f85270fba03dc10065313ca99a921926f350b03
Balázs Orbán <info@balazsorban.com>
2020-03-10 09:51:27 +0100

    test: remove duplicate test file

--- src/__tests__/static.test.js
+++
@@ -1,8 +0,0 @@
-import * as page from '../../tests/pages/static'
-import {render} from '..'
-
-test('renders with getStaticProps', async () => {
-  const {container} = await render(page)
-
-  expect(container).toHaveTextContent(/id: 1/)
-})
